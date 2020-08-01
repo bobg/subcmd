60fffb8b342a37650c58329822f906c145599490
Bob Glickstein <bobg@emphatic.com>
2020-08-01 10:15:33 -0700

    Add detail to error messages.

--- subcmd.go
+++ subcmd.go
@@ -3,6 +3,7 @@ package subcmd
 import (
 	"context"
 	"flag"
+	"sort"
 
 	"github.com/pkg/errors"
 )
@@ -20,15 +21,22 @@ var (
 
 // Run runs the subcommand of c in args[0], passing the rest of args as its arguments.
 func Run(ctx context.Context, c Cmd, args []string) error {
+	cmds := c.Subcmds()
+
+	var cmdnames sort.StringSlice
+	for cmdname := range cmds {
+		cmdnames = append(cmdnames, cmdname)
+	}
+	cmdnames.Sort()
+
 	if len(args) == 0 {
-		return ErrNoSubcommand
+		return errors.Wrapf(ErrNoSubcommand, "want one of %v", cmdnames)
 	}
 	subcmd := args[0]
 	args = args[1:]
-	cmds := c.Subcmds()
 	f, ok := cmds[subcmd]
 	if !ok {
-		return errors.Wrap(ErrBadSubcommand, subcmd)
+		return errors.Wrapf(ErrBadSubcommand, "unknown subcommand %s, want one of %v", subcmd, cmdnames)
 	}
 	err := f(ctx, flag.NewFlagSet("", flag.ContinueOnError), args)
 	return errors.Wrapf(err, "running %s", subcmd)
